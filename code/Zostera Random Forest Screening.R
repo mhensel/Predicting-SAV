f5958fc421e8cd1af7a9eb1d0e38ef9203227b0a
Jon Lefcheck <lefcheckj@si.edu>
2021-07-28 10:57:32 -0400

    added GAM for Zost

--- code/Zostera Random Forest Screening.R
+++ code/Zostera Random Forest Screening.R
@@ -1,6 +1,7 @@
 
 # Load required libraries
 library(here)
+library(mgcViz) 
 library(tidyverse)
 library(randomForest)
 
@@ -23,3 +24,49 @@ rownames(model_importance) <- NULL
 model_importance$group <-as.factor( gsub("(.*)\\..*", "\\1", model_importance$variable))
 
 model_importance %>% group_by(group) %>% summarize(important = variable[which.max(IncNodePurity)], importance = IncNodePurity[which.max(IncNodePurity)])
+
+# Put together models
+library(piecewiseSEM)
+library(mgcv)
+
+eelgrass_model <- gam(dens.percomp.change ~ 
+                        s(dens.percomp.y1) +
+                        s(TN.me) +
+                        s(TP.me) +
+                        s(Temp.growy1max) +
+                        # s(TSS.sumDme) +
+                        s(Sal.y1me) +
+                        s(Secc.me) +
+                        s(Chla.me) +
+                        s(year),
+                      data = data)
+
+gam.check(eelgrass_model)                        
+
+summary(eelgrass_model)  
+
+plot(sm(getViz(eelgrass_model), 6))
+
+light_model <- gam(Secc.me ~ s(Chla.me) +
+                     s(year),
+                   data = data)
+
+gam.check(light_model)
+
+chla_model <- gam(Chla.me ~ 
+                    s(TN.me) +
+                    s(TP.me) +
+                    s(Temp.me) +
+                    s(year),
+                  data = data)
+
+gam.check(chla_model)
+
+# Put models into SEM
+model <- psem(
+  eelgrass_model,
+  light_model,
+  chla_model,
+  data = data)
+
+summary(model)
